868e0c022ae8fe515b1754bf239bf56b92deb67d
Zon3rd <artemiszbobylev05@gmail.com>
2023-11-25 01:51:23 +0600

    bmvgvn

---
+++ function.py
@@ -0,0 +1,5 @@
+import main
+from main import get_response
+
+
+get_response()
--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print('fggvnm')
-print('hvgvv')
+import requests
+from bs4 import BeautifulSoup
+import config
+
+def get_response():
+    url = config.url
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Теперь вы можете использовать BeautifulSoup для извлечения информации из HTML-кода
+    # Например, вы можете извлечь заголовки объявлений:
+        titles = soup.find_all('a', class_='marginright5 link linkWithHash detailsLink')
+
+        for title in titles:
+            print(title.text.strip())
+    else:
+        print(f"Ошибка запроса: {response.status_code}")
+
